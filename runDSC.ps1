d94555b836ea0d7a551ed478952fa7d504ca47ba
Florian H <fhedtstueck@posteo.de>
2017-11-14 14:21:01 +0100

    Initial Commit

---
+++ runDSC.ps1
@@ -0,0 +1,58 @@
+# To find the Thumbprint for an installed SSL certificate for use with the pull server list all certificates in your local store 
+# and then copy the thumbprint for the appropriate certificate by reviewing the certificate subjects
+#dir Cert:\LocalMachine\my
+
+# Then include this thumbprint when running the configuration
+#Sample_xDSCPullServer -certificateThumbprint 'A7000024B753FA6FFF88E966FD6E19301FAE9CCC' -RegistrationKey '140a952b-b9d6-406b-b416-e0f759c9c0e4' -OutputPath c:\Configs\PullServer
+
+# Run the compiled configuration to make the target node a DSC Pull Server
+#Start-DscConfiguration -Path c:\Configs\PullServer -Wait -Verbose
+
+configuration Sample_xDscPullServer
+{ 
+    param  
+    ( 
+            [string[]]$NodeName = 'localhost', 
+
+            [ValidateNotNullOrEmpty()] 
+            [string] $certificateThumbPrint,
+
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string] $RegistrationKey 
+     ) 
+
+     Import-DSCResource -ModuleName xPSDesiredStateConfiguration
+     Import-DSCResource –ModuleName PSDesiredStateConfiguration
+
+     Node $NodeName 
+     { 
+         WindowsFeature DSCServiceFeature 
+         { 
+             Ensure = 'Present'
+             Name   = 'DSC-Service'             
+         } 
+
+         xDscWebService PSDSCPullServer 
+         { 
+             Ensure                   = 'Present' 
+             EndpointName             = 'PSDSCPullServer' 
+             Port                     = 8080 
+             PhysicalPath             = "$env:SystemDrive\inetpub\PSDSCPullServer" 
+             CertificateThumbPrint    = $certificateThumbPrint          
+             ModulePath               = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules" 
+             ConfigurationPath        = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Configuration" 
+             State                    = 'Started'
+             DependsOn                = '[WindowsFeature]DSCServiceFeature'     
+             UseSecurityBestPractices = $false
+         } 
+
+        File RegistrationKeyFile
+        {
+            Ensure          = 'Present'
+            Type            = 'File'
+            DestinationPath = "$env:ProgramFiles\WindowsPowerShell\DscService\RegistrationKeys.txt"
+            Contents        = $RegistrationKey
+        }
+    }
+}
